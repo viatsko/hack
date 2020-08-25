77b331513932c6b6f0ea1cb53a0588f7f82128c4
Valerii Iatsko <viatsko@viatsko.me>
2020-08-26 00:51:52 +0200

    + LeetCode 136. Single Number

--- leetcode/136-single-number/SingleNumber.java
+++ leetcode/136-single-number/SingleNumber.java
@@ -1,9 +1,11 @@
 class Solution {
-    public int singleNumber(int[] nums) {
-        int result = 0;
-        for (int num : nums) {
-            result ^= num;
-        }
-        return result;
+  public int singleNumber(int[] nums) {
+    int result = 0;
+
+    for (int num : nums) {
+      result ^= num;
     }
+
+    return result;
+  }
 }
