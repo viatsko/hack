2b8be0012581e5c569b04b5085b97ed09b0bab2d
Valerii Iatsko <viatsko@viatsko.me>
2020-08-28 15:10:24 +0200

    + LeetCode 1266. Minimum Time Visiting All Points

---
+++ leetcode/1266-minimum-time-visiting-all-points/MinimumTimeVisitingAllPoints.java
@@ -0,0 +1,11 @@
+class Solution {
+  public int minTimeToVisitAllPoints(int[][] points) {
+    int result = 0;
+
+    for (int i = 1; i < points.length; i++) {
+      result += Math.max(Math.abs(points[i][0] - points[i - 1][0]), Math.abs(points[i][1] - points[i - 1][1]));
+    }
+
+    return result;
+  }
+}
