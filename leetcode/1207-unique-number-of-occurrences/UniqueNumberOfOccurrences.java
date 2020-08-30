e199508f62944715a9fc4a65f51135eaa1c21f1d
Valerii Iatsko <viatsko@viatsko.me>
2020-08-30 02:24:39 +0200

    + LeetCode 1207. Unique Number of Occurrences

---
+++ leetcode/1207-unique-number-of-occurrences/UniqueNumberOfOccurrences.java
@@ -0,0 +1,14 @@
+class Solution {
+  public boolean uniqueOccurrences(int[] arr) {
+    Map <Integer, Integer> map = new HashMap<>();
+
+    for (int i = 0; i < arr.length; i++) {
+      int count = map.getOrDefault(arr[i], 0);
+      map.put(arr[i], count + 1);
+    }
+
+    Set<Integer> set = new HashSet<>(map.values());
+
+    return set.size() == map.values().size();
+  }
+}
