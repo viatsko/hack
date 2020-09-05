a9906be2072b48d025f2897b3c53d9f5626ea0ea
Valerii Iatsko <viatsko@viatsko.me>
2020-09-05 15:52:26 +0200

    + LeetCode 1305. All Elements in Two Binary Search Trees

--- leetcode/1305-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.js
+++ leetcode/1305-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.js
@@ -12,34 +12,38 @@
  * @return {number[]}
  */
 var getAllElements = function (root1, root2) {
+  const stack1 = [];
+  const stack2 = [];
+
   const res = [];
-
-  const s1 = [];
-  const s2 = [];
-
-  while (root1 || root2 || s1.length || s2.length) {
+  while (root1 || root2 || stack1.length || stack2.length) {
     while (root1) {
-      s1.push(root1);
+      stack1.push(root1);
       root1 = root1.left;
     }
 
     while (root2) {
-      s2.push(root2);
+      stack2.push(root2);
       root2 = root2.left;
     }
 
     if (
-      !s2.length ||
-      (s1.length && s1[s1.length - 1].val <= s2[s2.length - 1].val)
+      stack1.length > 0 &&
+      (stack2.length === 0 ||
+        stack1[stack1.length - 1].val < stack2[stack2.length - 1].val)
     ) {
-      const el = s1.pop();
+      const el = stack1.pop();
+
       res.push(el.val);
+
       if (el.right) {
         root1 = el.right;
       }
     } else {
-      const el = s2.pop();
+      const el = stack2.pop();
+
       res.push(el.val);
+
       if (el.right) {
         root2 = el.right;
       }
