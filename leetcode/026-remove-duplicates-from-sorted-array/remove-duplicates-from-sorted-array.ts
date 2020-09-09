1ee840f93f830b043c9d8883ce10604c1d4471c9
Valerii Iatsko <viatsko@viatsko.me>
2020-09-09 18:56:32 +0200

    + LeetCode 026. Remove Duplicates from Sorted Array

---
+++ leetcode/026-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.ts
@@ -0,0 +1,10 @@
+function removeDuplicates(nums: number[]): number {
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] === nums[i - 1]) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+
+  return nums.length;
+}
