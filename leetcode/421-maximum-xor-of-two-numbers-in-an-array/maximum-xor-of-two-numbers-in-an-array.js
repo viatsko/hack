0dcd225014d53b596edcbb652ef3992662d87eca
Valerii Iatsko <viatsko@viatsko.me>
2020-09-16 14:44:57 +0200

    + LeetCode 421. Maximum XOR of Two Numbers in an Array

--- leetcode/421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.js
+++ leetcode/421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.js
@@ -2,7 +2,7 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findMaximumXOR = function (nums) {
+const findMaximumXOR = function (nums) {
   let max = 0;
 
   for (let i = 0; i < nums.length - 1; i++) {
