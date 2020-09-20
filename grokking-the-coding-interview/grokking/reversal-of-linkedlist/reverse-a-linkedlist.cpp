e8db6a193e813d0ecd38f1f36cd1d4230f66ada3
Valerii Iatsko <viatsko@viatsko.me>
2020-09-21 00:51:55 +0200

    + Reverse a LinkedList

---
+++ grokking-the-coding-interview/grokking/reversal-of-linkedlist/reverse-a-linkedlist.cpp
@@ -0,0 +1,43 @@
+using namespace std;
+
+#include <iostream>
+
+class ListNode {
+ public:
+  int value = 0;
+  ListNode *next;
+
+  ListNode(int value) {
+    this->value = value;
+    next = nullptr;
+  }
+};
+
+class ReverseLinkedList {
+ public:
+  static ListNode *reverse(ListNode *head) {
+    ListNode* newHead = nullptr;
+    while(head) {
+      ListNode* next = head->next;
+      head->next = newHead;
+      newHead = head;
+      head = next;
+    }
+    return newHead;
+  }
+};
+
+int main(int argc, char *argv[]) {
+  ListNode *head = new ListNode(2);
+  head->next = new ListNode(4);
+  head->next->next = new ListNode(6);
+  head->next->next->next = new ListNode(8);
+  head->next->next->next->next = new ListNode(10);
+
+  ListNode *result = ReverseLinkedList::reverse(head);
+  cout << "Nodes of the reversed LinkedList are: ";
+  while (result != nullptr) {
+    cout << result->value << " ";
+    result = result->next;
+  }
+}
