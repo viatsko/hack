3534633bfaa4ed7d2e790130cebdf4abdd381869
Valerii Iatsko <viatsko@viatsko.me>
2020-09-20 18:25:22 +0200

    + LinkedList Cycle

---
+++ grokking-the-coding-interview/grokking/two-pointers/linkedlist-cycle.cpp
@@ -0,0 +1,49 @@
+using namespace std;
+
+#include <iostream>
+
+class ListNode {
+ public:
+  int value = 0;
+  ListNode *next;
+
+  ListNode(int value) {
+    this->value = value;
+    next = nullptr;
+  }
+};
+
+class LinkedListCycle {
+ public:
+  static bool hasCycle(ListNode *head) {
+    ListNode *slow = head;
+    ListNode *fast = head;
+
+    while (fast != nullptr && fast->next != nullptr) {
+      slow = slow->next;
+      fast = fast->next->next;
+
+      if (slow == fast) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+};
+
+int main(int argc, char *argv[]) {
+  ListNode *head = new ListNode(1);
+  head->next = new ListNode(2);
+  head->next->next = new ListNode(3);
+  head->next->next->next = new ListNode(4);
+  head->next->next->next->next = new ListNode(5);
+  head->next->next->next->next->next = new ListNode(6);
+  cout << "LinkedList has cycle: " << LinkedListCycle::hasCycle(head) << endl;
+
+  head->next->next->next->next->next->next = head->next->next;
+  cout << "LinkedList has cycle: " << LinkedListCycle::hasCycle(head) << endl;
+
+  head->next->next->next->next->next->next = head->next->next->next;
+  cout << "LinkedList has cycle: " << LinkedListCycle::hasCycle(head) << endl;
+}
