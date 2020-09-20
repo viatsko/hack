070ab5d2ae96c536abe4877b9f5d70b5c98db54b
Valerii Iatsko <viatsko@viatsko.me>
2020-09-20 18:28:34 +0200

    + Start of LinkedList Cycle

---
+++ grokking-the-coding-interview/grokking/fast-and-slow-pointers/start-of-linkedlist-cycle.cpp
@@ -0,0 +1,56 @@
+using namespace std;
+
+#include <iostream>
+
+class ListNode {
+ public:
+  int value = 0;
+  ListNode *next;
+
+  ListNode(int value) {
+    this->value = value;
+    next = nullptr;
+  }
+};
+
+class LinkedListCycleStart {
+ public:
+  static ListNode *findCycleStart(ListNode *head) {
+    ListNode *slow = head;
+    ListNode *fast = head;
+
+    while (fast != nullptr && fast->next != nullptr) {
+      slow = slow->next;
+      fast = fast->next->next;
+
+      if (slow == fast) {
+        while (head != slow) {
+          head = head->next;
+          slow = slow->next;
+        }
+
+        return head;
+      }
+    }
+
+    return nullptr;
+  }
+};
+
+int main(int argc, char *argv[]) {
+  ListNode *head = new ListNode(1);
+  head->next = new ListNode(2);
+  head->next->next = new ListNode(3);
+  head->next->next->next = new ListNode(4);
+  head->next->next->next->next = new ListNode(5);
+  head->next->next->next->next->next = new ListNode(6);
+
+  head->next->next->next->next->next->next = head->next->next;
+  cout << "LinkedList cycle start: " << LinkedListCycleStart::findCycleStart(head)->value << endl;
+
+  head->next->next->next->next->next->next = head->next->next->next;
+  cout << "LinkedList cycle start: " << LinkedListCycleStart::findCycleStart(head)->value << endl;
+
+  head->next->next->next->next->next->next = head;
+  cout << "LinkedList cycle start: " << LinkedListCycleStart::findCycleStart(head)->value << endl;
+}
