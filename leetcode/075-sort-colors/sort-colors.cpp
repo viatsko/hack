f651178b2f4cfb7d712c9d83eb0113c52c0fe7a3
Valerii Iatsko <viatsko@viatsko.me>
2020-09-20 16:34:06 +0200

    + LeetCode 75. Sort Colors

---
+++ leetcode/075-sort-colors/sort-colors.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+  void sortColors(vector<int>& nums) {
+    int left = 0;
+    int right = nums.size() - 1;
+    for (int i = 0; i <= right;) {
+      if (nums[i] == 0) {
+        swap(nums[i], nums[left]);
+        i++;
+        left++;
+      } else if (nums[i] == 1) {
+        i++;
+      } else {
+        swap(nums[i], nums[right]);
+        right--;
+      }
+    }
+  }
+};
