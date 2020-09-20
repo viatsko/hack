7eda67ac534c5540a931d506845f45168c695257
Valerii Iatsko <viatsko@viatsko.me>
2020-09-20 15:01:47 +0200

    + LeetCode 16. 3Sum Closest

---
+++ leetcode/016-3sum-closest/three-sum-closest.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+  int threeSumClosest(vector<int>& arr, int targetSum) {
+    int closestSum = 0;
+    int minDiff = numeric_limits<int>::max();
+
+    sort(arr.begin(), arr.end());
+
+    for (int i = 0; i < arr.size() - 2; i++) {
+      int left = i + 1;
+      int right = arr.size() - 1;
+      while (left < right) {
+        int sum = arr[left] + arr[right] + arr[i];
+
+        if (abs(sum - targetSum) < minDiff) {
+          minDiff = abs(sum - targetSum);
+          closestSum = sum;
+        }
+
+        if (sum > targetSum) {
+          right--;
+        } else {
+          left++;
+        }
+      }
+    }
+
+    return closestSum;
+  }
+};
