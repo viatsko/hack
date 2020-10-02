0886a573c149af8b0c6b197c4ce3fae7a493a9b1
Valerii Iatsko <viatsko@viatsko.me>
2020-10-02 06:18:51 +0200

    + LeetCode 892. Surface Area of 3D Shapes

---
+++ leetcode/892-surface-area-of-3d-shapes/surface-area-of-3d-shapes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+  int surfaceArea(vector<vector<int>> &grid) {
+    int result = 0;
+
+    for (int i = 0; i < grid.size(); i++) {
+      for (int j = 0; j < grid[0].size(); j++) {
+        if (grid[i][j])
+          result += grid[i][j] * 4 + 2;
+        if (i > 0)
+          result -= min(grid[i][j], grid[i - 1][j]) * 2;
+        if (j > 0)
+          result -= min(grid[i][j], grid[i][j - 1]) * 2;
+      }
+    }
+
+    return result;
+  }
+};
