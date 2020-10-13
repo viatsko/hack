4ab31e6ac2b93a59c2466795b7489b52c38ec251
Valerii Iatsko <viatsko@viatsko.me>
2020-10-13 14:54:35 +0200

    Updated letter combinations

--- leetcode/017-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.js
+++ leetcode/017-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.js
@@ -1,35 +1,36 @@
-const letterCombinations = digits => {
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+const letterCombinations = function(digits) {
     if (digits.length === 0) {
         return [];
     }
 
-    const c = new Map();
-    c.set('*', ['+']);
-    c.set('0', [' ']);
-    c.set('1', []);
-    c.set('2', ['a', 'b', 'c']);
-    c.set('3', ['d', 'e', 'f']);
-    c.set('4', ['g', 'h', 'i']);
-    c.set('5', ['j', 'k', 'l']);
-    c.set('6', ['m', 'n', 'o']);
-    c.set('7', ['p', 'q', 'r', 's']);
-    c.set('8', ['t', 'u', 'v']);
-    c.set('9', ['w', 'x', 'y', 'z']);
+    const mp = {};
+    mp[2] = 'abc'.split('');
+    mp[3] = 'def'.split('');
+    mp[4] = 'ghi'.split('');
+    mp[5] = 'jkl'.split('');
+    mp[6] = 'mno'.split('');
+    mp[7] = 'pqrs'.split('');
+    mp[8] = 'tuv'.split('');
+    mp[9] = 'wxyz'.split('');
 
-    const d = digits.split('');
-    const result = [];
+    let result = [
+        []
+    ];
 
-    const combo = (s, d, idx) => {
-        if (d.length === idx) {
-            return result.push(s);
+    for (const digit of digits) {
+        prev = result;
+        result = [];
+
+        for (const letter of mp[+digit]) {
+            for (const subResult of prev) {
+                result.push(subResult + letter);
+            }
         }
-
-        for (let i = 0; i < c.get(d[idx]).length; i++) {
-            combo(s + c.get(d[idx])[i], d, idx + 1);
-        }
-    };
-
-    combo('', d, 0);
+    }
 
     return result;
 };
